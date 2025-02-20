7984f1bee619ffb4b6616c20134f2c2b3923ee61
Aliahmed2k03 <ali.ahmed-18@students.plymouth.ac.uk>
2025-02-20 20:20:19 +0000

    added google maps API and got map working on locations page

--- frontend/src/Components/Locations.jsx
+++ frontend/src/Components/Locations.jsx
@@ -1,5 +1,14 @@
+"use client";
+
 import { useState } from "react";
 import Sidebar from "./Sidebar";
+import {
+    APIProvider,
+    Map,
+    AdvancedMarker,
+    Pin,
+    InfoWindow,
+} from "@vis.gl/react-google-maps";
 
 
 function Location(){
@@ -9,6 +18,12 @@ function Location(){
             <div className="absolute top-3 left-60 w-255     h-16 px-6 py-3 bg-white shadow-lg rounded-2xl">
                 <span className="absolute text-black font-bold text-4xl top-2.6 left-4 cursor-default">Locations</span>
             </div>
+            <APIProvider apiKey="AIzaSyCZPYOASUrajNHwTK2WJO2bozseJhR0g-w">
+                <div className="w-[75%] h-[75vh] absolute top-26 left-70 rounded-lg">
+                <Map zoom={15} options={{gestureHandling: "greedy",  zoomControl: true, streetViewControl: false,  fullscreenControl: false }}> 
+                </Map>
+                </div>
+            </APIProvider>
         </div>
     );
 };
