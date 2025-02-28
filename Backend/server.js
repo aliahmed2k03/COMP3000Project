354fc7c32233bcd61ab6106a5ddd77e01e223f90
aliahmed2k03 <ali.ahmed-18@students.plymouth.ac.uk>
2025-02-28 12:01:02 +0000

    added scraped house data to frontend

--- Backend/server.js
+++ Backend/server.js
@@ -43,24 +43,34 @@ app.post('/register',async (req,res) => {
     }
 });
 
-async function scrapeZoopla() {
-    const url = "https://www.zoopla.co.uk/for-sale/details/69524047/?search_identifier=d596ed4ede7f8b13259e087cd518478b33e4ac98156a80ea577e24746240c890"
+app.get('/scrapeZoopla',async (req,res)=>{
+    const url = "https://www.zoopla.co.uk/for-sale/details/66154965/?search_identifier=d596ed4ede7f8b13259e087cd518478b33e4ac98156a80ea577e24746240c890&featured=1&utm_content=featured_listing"
     try{
+        console.log("grabbing house address and price...")
         const browser = await puppeteer.launch({ headless: true});
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36');
         await page.goto(url, { waitUntil: 'networkidle2' });
         const address = await page.$eval('address._1olqsf98', el => el.textContent.trim());
         const price = await page.$eval('p._194zg6t3.r4q9to1', el => el.textContent.trim());
-        console.log({ address, price });
+        const estate = await page.$eval('p._194zg6t7._133vwz72', el => el.textContent.trim());
+        const beds = await page.$eval("p._194zg6t8._1wmbmfq3", el => el.textContent.trim());
+        const baths = await page.$$eval("p._194zg6t8._1wmbmfq3",elements =>{
+            const bathtext = elements.find(el => el.textContent.includes('bath'))
+            return bathtext ? bathtext.textContent.trim(): null;
+        });
         await browser.close();
+        console.log({ address, price , estate,beds,baths})
+        res.json({ address, price , estate,beds,baths});
     }catch(error){
         console.log("Error",error.message);
+        res.status(500).json({ error: "Failed to scrape data" });
     }
+}); 
     
-}
+    
+
 
 app.listen(5000, ()=>{
-    scrapeZoopla();
     console.log('Server is running on 5000');
 });
--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
+import axios from 'axios';
 
 
 function Dashboard(){
+    const [scrapedHouse,setScrapedHouse] = useState(null);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
+
+    useEffect(() =>{
+        axios.get("http://localhost:5000/scrapeZoopla")
+        .then(response =>{
+            setScrapedHouse(response.data);
+            setLoading(false);
+        })
+        .catch(err =>{
+            console.error("error", err);
+            setError("Failed to load house");
+            setLoading(false);
+        })
+    },[]);
+
+    if (loading) return<p>loading house details...</p>;
+    if (error) return <p>{error}</p>
     return (
         <div className="  bg-gray-100 h-screen w-screen">
             <Sidebar />
             <div className="absolute top-3 left-60 right-5 h-16 px-6 py-3 bg-white shadow-lg rounded-2xl">
                 <span className="absolute text-black font-bold text-4xl top-2.6 left-4 cursor-default">Dashboard</span>
+            </div>
+            <div className="flex flex-col justify-center absolute top-23 left-60 w-60 h-120 px-6 py-5 bg-white shadow-lg rounded-2xl">
+            <span className="text-2x1 text-center py-4 text-black break-words ">{scrapedHouse.address}</span>
+            <span className="text-2x1 text-center py-4 text-black break-words ">{scrapedHouse.beds}</span>
+            <span className="text-2x1 text-center py-4 text-black break-words ">{scrapedHouse.baths}</span>
+            <span className="text-2x1 text-center py-4 text-black break-words ">{scrapedHouse.estate}</span>
+            <span className="text-2x1 text-center py-4 text-black break-words ">{scrapedHouse.price}</span>  
             </div>
         </div>
     );
